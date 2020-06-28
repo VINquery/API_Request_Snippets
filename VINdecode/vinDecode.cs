30cd2170b06b0ec8b166a80935abf756621383e0
Sone <sophie.s@vinquery.com>
2020-06-28 12:11:13 -0400

    Uploaded.

---
+++ VINdecode/vinDecode.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
+
+namespace VINdecode
+{
+    class vinDecode
+    {
+        static void Main(string[] args)
+        {
+            String url = "http://ws.vinquery.com/restxml.aspx?accesscode=YOUR_ACCESS_CODE&reportType=YOUR_REPORT_TYPE&vin=YOUR_VIN";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader stream = new StreamReader(dataStream);
+                String responseString = stream.ReadToEnd();
+                Console.WriteLine(responseString);
+
+                XElement root = XElement.Parse(responseString);
+                Console.WriteLine(root);
+
+                IEnumerable<XElement> tests =
+                    from el in root.Elements("VIN").Elements("Vehicle").Elements("Item")
+                    where (string) el.Attribute("Key") != null
+                    select el;
+                foreach (XElement el in tests)
+                    Console.WriteLine((string) el.Attribute("Key") + ": " + (string) el.Attribute("Value"));
+            }
+
+            response.Close();
+
+        }
+    }
+}
