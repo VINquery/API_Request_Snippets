62d34968324ff25d57e4f55a3871ee237f674468
Sone <sophie.s@vinquery.com>
2020-07-06 13:56:17 -0400

    Simplified

--- VINfix/vinFIX.js
+++ VINfix/vinFIX.js
@@ -1,73 +1,61 @@
 const url = "http://www.recognition.ws/vinfix/v1?accesscode=ACCESS_CODE&vin=YOUR_VIN";
 
+// Synchronous Get HTTP Request
 function httpGetSync() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, false);
     xhttp.send();
     parser = new DOMParser();
     xmlDoc = parser.parseFromString(xhttp.responseText,"text/xml");
-    var text = "Algorithm 1: \n";
-    var list = xmlDoc.getElementsByTagName("Item");
-    var change = 0;
-    var length = 0;
+    var text;
+    var algorithm1 = xmlDoc.getElementsByTagName("Item")[0].getElementsByTagName("Algorithm1");
+    var algorithm2 = xmlDoc.getElementsByTagName("Item")[0].getElementsByTagName("Algorithm2");
 
-    for (var i = 0; i < list.length; i++) {
-        if(change == 0) {
-            if (list[i + 1].getAttribute("Key") == "Suggestion1") {
-                text += list[i].getAttribute("Key") + ": ";
-                text += list[i].getAttribute("Value") + "\n";
-                text += "Algorithm 2: \n";
-                change++;
-            }
-            else {
-                text += list[i].getAttribute("Key") + ": ";
-                text += list[i].getAttribute("Value") + "\n";
-            }
-        }
-        else if (change == 1) {
-            if (length < list.length) {
-                text += list[i].getAttribute("Key") + ": ";
-                text += list[i].getAttribute("Value") + "\n";
-            }
-        }
-        length++;
+    // As algorithm one and two can have varying amounts suggestions, this accomodates for any scenario.
+    
+    text = "Algorithm 1:";
+    for (var i = 0; i < algorithm1; i++) {
+      text += "\n" + algorithm1[i].getAttribute("Key") + ": ";
+      text += algorithm2[i].getAttribute("Value");
     }
+
+    text += "\nAlgorithm 2:"
+
+    for (var j = 0; j < algorithm2; j++) {
+      text += "\n" + algorithm2[j].getAttribute("Key") + ": ";
+      text += algorithm2[j].getAttribute("Value");
+    }
+
+    // Output text.
+
   }
   
+  // Asynchronous Get HTTP Response
   function httpGetAsync() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, true);
+    xhttp.open("GET", url, false);
     xhttp.send();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        parser = new DOMParser();
-        xmlDoc = parser.parseFromString(xhttp.responseText,"text/xml");
-        var text = "Algorithm 1: \n";
-        var list = xmlDoc.getElementsByTagName("Item");
-        var change = 0;
-        var length = 0;
+    parser = new DOMParser();
+    
+    xhttp.onreadystatechange = function() {
+      xmlDoc = parser.parseFromString(xhttp.responseText,"text/xml");
+      var text;
+      var algorithm1 = xmlDoc.getElementsByTagName("Item")[0].getElementsByTagName("Algorithm1");
+      var algorithm2 = xmlDoc.getElementsByTagName("Item")[0].getElementsByTagName("Algorithm2");
 
-        for (var i = 0; i < list.length; i++) {
-            if(change == 0) {
-                if (list[i + 1].getAttribute("Key") == "Suggestion1") {
-                  text += list[i].getAttribute("Key") + ": ";
-                  text += list[i].getAttribute("Value") + "\n";
-                  text += "Algorithm 2: \n";
-                  change++;
-              }
-              else {
-                text += list[i].getAttribute("Key") + ": ";
-                text += list[i].getAttribute("Value") + "\n";
-              }
-            }
-            else if (change == 1) {
-              if (length < list.length) {               
-                text += list[i].getAttribute("Key") + ": ";
-                text += list[i].getAttribute("Value") + "\n";
-            }
-          }
-          length++;
-        }
+      // As algorithm one and two can have varying amounts suggestions, this accomodates for any scenario.
+    
+      text = "Algorithm 1:";
+      for (var i = 0; i < algorithm1; i++) {
+        text += "\n" + algorithm1[i].getAttribute("Key") + ": ";
+        text += algorithm2[i].getAttribute("Value");
+      }
+
+      text += "\nAlgorithm 2:"
+
+      for (var j = 0; j < algorithm2; j++) {
+        text += "\n" + algorithm2[j].getAttribute("Key") + ": ";
+        text += algorithm2[j].getAttribute("Value");
       }
     }
   }
